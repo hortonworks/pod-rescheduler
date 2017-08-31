68108ba3e9f13d9ebbf4063a3a756590a4615fe9
Krisztian Horvath <keyki.kk@gmail.com>
2017-08-31 14:58:10 +0200

    use logger

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"path/filepath"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
@@ -37,10 +37,9 @@ func main() {
 	nodeClient := clientSet.Nodes()
 
 	for {
-		// nodes
 		nodes, err := nodeClient.List(metav1.ListOptions{})
 		if err != nil {
-			fmt.Println("Node list error: ", err.Error())
+			log.Infof("Node list error: %s", err.Error())
 			continue
 		}
 
@@ -58,8 +57,7 @@ func main() {
 		for group, pods := range podGroups {
 			if movablePod := findMovablePod(pods); movablePod != nil {
 				if node := findNodeForPod(podsPerNode, group, nodes.Items); node != nil {
-					fmt.Printf("Attempting to move Pod (%s) to node %s\n", movablePod.Name, node.Name)
-
+					log.Infof("Attempting to move Pod (%s) to node %s", movablePod.Name, node.Name)
 
 				}
 			}
@@ -90,7 +88,7 @@ func findMovablePod(pods []corev1.Pod) *corev1.Pod {
 		if pod.Status.Phase == corev1.PodRunning {
 			node := pod.Spec.NodeName
 			if len(podsByNode[node]) == 1 {
-				fmt.Printf("Pod: %s can be moved as there is another running pod (%s) on the same node: %s\n", pod.Name, podsByNode[node][0].Name, node)
+				log.Infof("Pod: %s can be moved as there is another running pod (%s) on the same node: %s", pod.Name, podsByNode[node][0].Name, node)
 				return &pod
 			}
 			podsByNode[node] = append(podsByNode[node], pod)
@@ -101,18 +99,18 @@ func findMovablePod(pods []corev1.Pod) *corev1.Pod {
 
 // Find a node which does not run any Pod from one Deployment/StatefulSet
 func findNodeForPod(podsPerNode map[string][]corev1.Pod, group string, nodes []corev1.Node) *corev1.Node {
-	fmt.Println("Find node for Pod group:", group)
+	log.Infof("Find node for Pod group: %s", group)
 	for node, pods := range podsPerNode {
 		podFoundForGroup := false
 		for _, pod := range pods {
 			if getPodGroupName(pod) == group {
-				fmt.Printf("Found Pod group(%s) on node: %s, searching..\n", group, node)
+				log.Infof("Found Pod group(%s) on node: %s, searching..", group, node)
 				podFoundForGroup = true
 				break
 			}
 		}
 		if !podFoundForGroup {
-			fmt.Printf("Foud node: %s for Pod group: %s\n", node, group)
+			log.Infof("Foud node: %s for Pod group: %s", node, group)
 			return findNode(node, nodes)
 		}
 	}
@@ -129,10 +127,10 @@ func findNode(name string, nodes []corev1.Node) *corev1.Node {
 }
 
 func listPodsOnNode(ListPodsOnNode func(opts metav1.ListOptions) (*corev1.PodList, error), node corev1.Node) []corev1.Pod {
-	fmt.Println("List Pods on node:", node.Name)
+	log.Infof("List Pods on node: %s", node.Name)
 	podsOnNode, err := ListPodsOnNode(metav1.ListOptions{FieldSelector: fields.SelectorFromSet(fields.Set{"spec.nodeName": node.Name}).String()})
 	if err != nil {
-		fmt.Println("Failed to list Pods on node:", node.Name)
+		log.Infof("Failed to list Pods on node: %s", node.Name)
 		return nil
 	}
 	return podsOnNode.Items
